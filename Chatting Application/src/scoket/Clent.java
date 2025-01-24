9e6ebd2387f67a8c3cf82d1a270cd72424a29bca
shivam2k4 <shivamkale2712k4@gmail.com>
2025-01-24 22:13:56 +0530

    Add files via upload

---
+++ Chatting Application/src/scoket/Clent.java
@@ -0,0 +1,24 @@
+package scoket;
+
+import java.io.IOException;
+import java.net.*;
+
+public class Clent {
+
+	public static void main(String[] args) {
+		
+		try {
+			Socket s=new Socket ("127.0.0.1",3000);
+			
+			System.out.println("Connected!");
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+
+}
